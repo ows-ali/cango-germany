79e5033a2e2dd5329eeb9c4b2a2776840c7c99f5
Owais Ali <owaisali@creativechaos.co>
2026-07-18 23:46:53 +0200

    remove alloweddevorigin flag for prod

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  allowedDevOrigins: ["192.168.0.27", "matchbox-cheating-escapade.ngrok-free.dev"],
   experimental: {
     serverActions: {
       bodySizeLimit: "2mb",
