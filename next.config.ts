4d584110075295306cca2a1f9a33d3b818c661ea
Owais Ali <owaisali@creativechaos.co>
2026-07-18 16:48:54 +0200

    allow app running on local net

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["192.168.0.27"],
   experimental: {
     serverActions: {
       bodySizeLimit: "2mb",
