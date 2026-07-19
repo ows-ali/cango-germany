28865f068a9983ad7ad3672794501cfb9a1b367d
Owais Ali <owaisali@creativechaos.co>
2026-07-19 22:03:59 +0200

    add github ci

--- src/__tests__/setup.tsx
+++ src/__tests__/setup.tsx
@@ -3,6 +3,20 @@ import { setupServer } from "msw/node";
 import { handlers, resetStores } from "./api/mocks/handlers";
 import { beforeAll, afterEach, afterAll, vi } from "vitest";
 
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: vi.fn().mockImplementation((query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(),
+    removeListener: vi.fn(),
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
+
 vi.mock("next-auth/react", () => ({
   useSession: vi.fn(() => ({
     data: {
